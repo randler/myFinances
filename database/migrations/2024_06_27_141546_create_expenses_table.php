bdcd2e5d27108681d8a713a4d53d21a83946e7a6
DiegoFerraz07 <dihferraz-01@hotmail.com>
2024-06-27 11:33:46 -0300

    up table expenses

---
+++ database/migrations/2024_06_27_141546_create_expenses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('food')->nullable();
+            $table->decimal('electrical_energy')->nullable();
+            $table->decimal('health')->nullable();
+            $table->decimal('internet')->nullable();
+            $table->decimal('credit_card')->nullable();
+            $table->decimal('condominium')->nullable();
+            $table->decimal('consortium')->nullable();
+            $table->decimal('rent')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expenses');
+    }
+};
