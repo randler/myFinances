8ac827e7d7186686a1dd2e83f62aaaad0b4e8129
DiegoFerraz07 <dihferraz-01@hotmail.com>
2024-07-02 21:44:26 -0300

    seeders create

---
+++ app/Filament/Resources/ExpensesResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ExpensesResource\Pages;
+use App\Filament\Resources\ExpensesResource\RelationManagers;
+use App\Models\Expenses;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ExpensesResource extends Resource
+{
+    protected static ?string $model = Expenses::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListExpenses::route('/'),
+            'create' => Pages\CreateExpenses::route('/create'),
+            'edit' => Pages\EditExpenses::route('/{record}/edit'),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,5 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(FinanceAssetsSeeder::class);
+        $this->call(ExpensesSeeder::class);
+        $this->call(InvestmentsSeeder::class);
     }
 }
