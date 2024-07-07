f19210295d0477c31bcaeeb15235208a65e3fade
Randler Ferraz Leal <randlersi@gmail.com>
2024-07-07 11:17:09 -0300

    adicionando funcionalidade dashboard

---
+++ app/Filament/Resources/ExpensesResource/Widgets/ExpensesOverview.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Filament\Resources\ExpensesResource\Widgets;
+
+use App\Repositories\ExpensesRepositories;
+use Filament\Support\RawJs;
+use Filament\Widgets\ChartWidget;
+
+class ExpensesOverview extends ChartWidget
+{
+    protected static ?string $heading = 'Despesas Planejadas do Mês';
+    protected static ?int $sort = 2;
+
+    protected function getData(): array
+    {
+        $repository = new ExpensesRepositories();
+        $planned = $repository->getMonthExpenses();
+        $paid = $repository->getMonthExpensesToPaid();
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Despesas Planejadas do Mês',
+                    'data' => [$planned, $paid],
+                    'backgroundColor' => ['#bd0b0b', '#17ad23'],
+                    'borderColor' => ['#bd0b0b','#17ad23'],
+                ],
+            ],
+            'labels' => ['Planejado', 'Pago'],
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'bar';
+    }
+
+    protected function getOptions(): RawJs
+    {
+        return RawJs::make(<<<JS
+        {
+            indexAxis: 'y',
+            scales: {
+                x: {
+
+                    ticks: {
+                        callback: (value) => 'R$' + value,
+                    },
+                },
+            },
+        }
+    JS);
+    }
+
+}
--- app/Filament/Resources/FinanceAssetsResource/Widgets/StatsAssets.php
+++ app/Filament/Resources/FinanceAssetsResource/Widgets/StatsAssets.php
@@ -3,19 +3,22 @@
 namespace App\Filament\Resources\FinanceAssetsResource\Widgets;
 
 use App\Models\FinanceAssets;
+use App\Repositories\ExpensesRepositories;
 use App\Repositories\FinanceAssetsRepositories;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class StatsAssets extends BaseWidget
 {
+    protected static ?int $sort = 0;
     protected function getStats(): array
     {
-        $repository = new FinanceAssetsRepositories();
-        $economy = $repository->getMonthEconomy();
+        $expensesRepository = new ExpensesRepositories();
+        $assetsRepository = new FinanceAssetsRepositories();
+        $economy = $assetsRepository->getMonthEconomy();
         $colorEconomy = $economy > 0 ? 'success' : 'danger';
         return [
-            Stat::make('Entradas do Mês', $repository->getMonthBalance())
+            Stat::make('Entradas do Mês', $assetsRepository->getMonthBalance())
                 ->description('Saldo total do mês')
                 ->descriptionIcon('heroicon-o-currency-dollar')
                 ->color('success'),
@@ -23,11 +26,11 @@ class StatsAssets extends BaseWidget
                 ->description('Economia total do mês')
                 ->descriptionIcon('heroicon-o-currency-dollar')
                 ->color($colorEconomy),
-            Stat::make('Despesas do mês', $repository->getMonthExpenses())
+            Stat::make('Despesas do mês', $expensesRepository->getMonthExpenses())
                 ->description('Total de despesas do mês')
                 ->descriptionIcon('heroicon-o-currency-dollar')
                 ->color('danger'),
-            Stat::make('Despesas Restantes', $repository->getMonthRemainingExpenses())
+            Stat::make('Despesas Restantes', $expensesRepository->getMonthExpensesToPaid())
                 ->description('Total de despesas restantes do mês')
                 ->descriptionIcon('heroicon-o-currency-dollar')
                 ->color('danger'),
